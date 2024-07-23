114a399240746d5ffd5ffc93a9d00ecf48ee2777
Ravjot Singh <ravjot313@yahoo.com>
2024-07-22 18:07:35 -0700

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+from langchain_google_genai import GoogleGenerativeAI
+
+app = Flask(__name__)
+api_key = "AIzaSyDMQUwZWlZlyyriw0YVZRDJ5A8hP-1X_tQ"
+
+llm = GoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=api_key)
+
+@app.route('/')
+def index():
+    return render_template('index1.html')
+
+def call_api(text):
+    return llm.invoke(text)
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    input_text = request.form['input_text']
+    output = call_api(input_text)
+    return render_template('index.html', input_text=input_text, generated_text=output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
